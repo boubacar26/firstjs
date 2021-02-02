4c3cc38e5618caa2fc17203035e140689a7b0be4
Oumar FALL <oumarfall@hotmail.fr>
2021-02-02 19:55:27 +0000

    corrections et ajout commentaires

--- testage.js
+++ testage.js
@@ -1,22 +1,18 @@
-//let age = 34;
-let age = prompt("Quel age avez vous?");
-age = parseInt(age);
+let age = prompt("Quel âge avez-vous?");
 
-console.debug(age);
+age = parseInt(age); // conversion de la saisie en nombre entier
 
-if(!Number.isInteger(age)){
+if(!Number.isInteger(age)){ // si la conversion réussit on continue
   console.info("Information erronée!");
 }
 else {
   if(age > 35){
-  console.info("Bienvenue");
-} else if(age >= 18 && age < 35) {
-  console.warn("Salut chef");
-}
-else {
-  console.warn("Acces interdit!");
-}
-
-  
+    console.info("Bienvenue");
+  } else if(age >= 18 && age < 35) {
+    console.warn("Salut chef!");
+  }
+  else {
+    console.warn("Acces interdit!");
+  }
 }
 
